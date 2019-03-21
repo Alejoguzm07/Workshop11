588eb0800497ac6639d588a974f3f16c880d4dd2
Alejoguzm07 <alejoguzm07@gmail.com>
2019-03-21 00:07:06 -0500

    api

---
+++ src/main/resources/static/js/apiclient.js
@@ -0,0 +1,9 @@
+apiclient=(function(){
+    return {
+		getCinemaByName: function(name,callback){            
+			$.get( "/cinemas/"+name, function( data ) {
+                callback(data);
+              });
+		}
+	}
+})();
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,12 +1,23 @@
 var module = (function () {
   var functions = {};
   var cinemaName = {};
-  var saveElements = function (cinema) {
-    functions = cinema[0].functions;
-    cinemaName = cinema[0].name;
+  var saveElements = function (cinema) {    
+    functions = cinema.functions;
+    cinemaName = cinema.name;
+  };
+  var updateFunctionsTable = function(){    
+    for (var i = 0; i < functions.length; i++) {
+      var fun = functions[i];
+      var txt = '<tr id="'+ (i + 1)+'">'+
+      '<th scope="row">'+cinemaName+'</th>'+
+      '<td>'+fun.name+'</td>'+
+      '<td>'+fun.seats+'</td>'+
+      '<td>'+fun.date+'<td>';
+      $("#funciones").append(txt);
+    };
   };
   var mapFunctions = function (cinema) {
-    saveElements(cinema);
+    saveElements(cinema);    
     var mappedFunctions = functions.map(function (fun) {
         var numeroSillas = 0;
         for (var i = 0; i < fun.seats.length; i++) {
@@ -15,10 +26,10 @@ var module = (function () {
         var newFunction = new Object();
         newFunction.name = fun.movie.name;
         newFunction.date = fun.date;
-        newFunction.seats = numeroSillas;
+        newFunction.seats = numeroSillas;        
     	return newFunction;
-    });
-    console.log(mappedFunctions);
+    });    
+    functions = mappedFunctions;
     return mappedFunctions;
   };
   return {
@@ -26,8 +37,14 @@ var module = (function () {
         this.cinemaName = newName;
     },
     saveElements:saveElements,
-    updateFunctions:function (cinemaName){
-        apimock.getCinemaByName(cinemaName,mapFunctions);
+    updateFunctions:function (){
+      $('#funciones').find('tbody').empty();    
+      var cinemaName = $('#cinemaName').val();      
+      if(cinemaName != ''){
+        //apimock.getCinemaByName(cinemaName,mapFunctions);
+        apiclient.getCinemaByName(cinemaName,mapFunctions);
+        updateFunctionsTable();
+      };      
     }
   };
 })();
